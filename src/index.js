a17183484d0e4297b8d034f8b8ef005e38507676
huskarnov <143254754+Huskarnov@users.noreply.github.com>
2025-06-25 10:30:45 +0100

    x

--- src/index.js
+++ src/index.js
@@ -77,8 +77,89 @@ class LinkedList {
   }
   contains(value) {
     let current = this.head;
+
+    if (current.value == value) {
+      return true;
+    }
+
     while (current.next) {
       current = current.next;
+      if (current.value == value) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let current = this.head;
+    let index = 1;
+
+    if (current.value == value) {
+      return index;
+    }
+
+    while (current.next) {
+      current = current.next;
+      index++;
+      if (current.value == value) {
+        return index;
+      }
+    }
+
+    return null;
+  }
+
+  toString() {
+    let current = this.head;
+    let stringRep = `( ${String(this.head.value)} ) -> `;
+
+    while (current.next) {
+      current = current.next;
+      stringRep += `( ${String(current.value)} ) -> `;
+    }
+
+    stringRep += "null";
+
+    return stringRep;
+  }
+
+  insertAt(value, index) {
+    if (index < 1 || index > this.size + 1) {
+      return alert("wrong index");
+    }
+
+    let current = this.head;
+    let count = 1;
+
+    if (index == 1) {
+      this.head = { value: value, next: current };
+    } else {
+      while (count !== index - 1) {
+        count++;
+        current = current.next;
+      }
+      current.next = { value: value, next: current.next };
+    }
+  }
+
+  removeAt(index) {
+    if (index < 1 || index > this.size) {
+      return alert("wrong index");
+    }
+    let current = this.head;
+    let count = 1;
+
+    if (index == 1) {
+      this.head = this.head.next;
+    } else {
+      while (count !== index - 1) {
+        count++;
+        current = current.next;
+      }
+
+      current.next = current.next.next;
     }
   }
 }
@@ -92,13 +173,11 @@ class Node {
 
 let list = new LinkedList();
 
-list.append(11);
-list.append(22);
-list.append(33);
-// list.append(44);
-// list.append(55);
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
-list.pop();
-list.getSize();
-
-console.log(list.at(2));
+console.log(list.toString());
